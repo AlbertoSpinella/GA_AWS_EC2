c78928c51c672b45fea7283454c8c0dba6a4274d
Alberto <alberto.spinella@protonmail.com>
2021-06-25 15:57:45 +0200

    Merge from refactoring branch

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,7 +1,7 @@
-apt -y update
+apt-get -y update
 
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-apt -y install nodejs
-apt -y install npm
+apt-get -y install nodejs
+apt-get -y install npm
 npm install -g pm2
 pm2 update
